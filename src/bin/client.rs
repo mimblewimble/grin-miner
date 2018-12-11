43ac6638a9e5e2c6c05220efaba9111d64870c1c
Quentin Le Sceller <q.lesceller@gmail.com>
2018-12-11 09:57:34 -0500

    Add support for secure stratum connection (#64)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -16,12 +16,13 @@
 //! stratum server
 
 use std;
-use std::io::{BufRead, ErrorKind, Write};
+use std::io::{self, BufRead, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::sync::{mpsc, Arc, RwLock};
 use std::thread;
 
 use bufstream::BufStream;
+use native_tls::{TlsConnector, TlsStream};
 use serde_json;
 use time;
 
@@ -34,12 +35,109 @@ pub enum Error {
 	ConnectionError(String),
 }
 
+struct Stream {
+	stream: Option<BufStream<TcpStream>>,
+	tls_stream: Option<BufStream<TlsStream<TcpStream>>>,
+}
+
+impl Stream {
+	fn new() -> Stream {
+		Stream {
+			stream: None,
+			tls_stream: None,
+		}
+	}
+	fn try_connect(&mut self, server_url: &str, tls: Option<bool>) -> Result<(), Error> {
+		match TcpStream::connect(server_url) {
+			Ok(conn) => {
+				if tls.is_some() && tls.unwrap() {
+					let connector = TlsConnector::new().unwrap();
+					let url_port: Vec<&str> = server_url.split(":").collect();
+					let splitted_url: Vec<&str> = url_port[0].split(".").collect();
+					let base_host = format!(
+						"{}.{}",
+						splitted_url[splitted_url.len() - 2],
+						splitted_url[splitted_url.len() - 1]
+					);
+					let mut stream = connector.connect(&base_host, conn).unwrap();
+					let _ = stream.get_mut().set_nonblocking(true);
+					self.tls_stream = Some(BufStream::new(stream));
+					Ok(())
+				} else {
+					let _ = conn.set_nonblocking(true);
+					self.stream = Some(BufStream::new(conn));
+					Ok(())
+				}
+			}
+			Err(e) => Err(Error::ConnectionError(format!("{}", e))),
+		}
+	}
+}
+
+impl Write for Stream {
+	fn write(&mut self, b: &[u8]) -> Result<usize, std::io::Error> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().write(b)
+		} else {
+			self.stream.as_mut().unwrap().write(b)
+		}
+	}
+	fn flush(&mut self) -> Result<(), std::io::Error> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().flush()
+		} else {
+			self.stream.as_mut().unwrap().flush()
+		}
+	}
+}
+impl Read for Stream {
+	fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().read(buf)
+		} else {
+			self.stream.as_mut().unwrap().read(buf)
+		}
+	}
+}
+
+impl BufRead for Stream {
+	fn fill_buf(&mut self) -> io::Result<&[u8]> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().fill_buf()
+		} else {
+			self.stream.as_mut().unwrap().fill_buf()
+		}
+	}
+	fn consume(&mut self, amt: usize) {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().consume(amt)
+		} else {
+			self.stream.as_mut().unwrap().consume(amt)
+		}
+	}
+	fn read_until(&mut self, byte: u8, buf: &mut Vec<u8>) -> io::Result<usize> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().read_until(byte, buf)
+		} else {
+			self.stream.as_mut().unwrap().read_until(byte, buf)
+		}
+	}
+	fn read_line(&mut self, string: &mut String) -> io::Result<usize> {
+		if self.tls_stream.is_some() {
+			self.tls_stream.as_mut().unwrap().read_line(string)
+		} else {
+			self.stream.as_mut().unwrap().read_line(string)
+		}
+	}
+}
+
 pub struct Controller {
 	_id: u32,
 	server_url: String,
 	server_login: Option<String>,
 	server_password: Option<String>,
-	stream: Option<BufStream<TcpStream>>,
+	server_tls_enabled: Option<bool>,
+	stream: Option<Stream>,
 	rx: mpsc::Receiver<types::ClientMessage>,
 	pub tx: mpsc::Sender<types::ClientMessage>,
 	miner_tx: mpsc::Sender<types::MinerMessage>,
@@ -52,6 +150,7 @@ impl Controller {
 		server_url: &str,
 		server_login: Option<String>,
 		server_password: Option<String>,
+		server_tls_enabled: Option<bool>,
 		miner_tx: mpsc::Sender<types::MinerMessage>,
 		stats: Arc<RwLock<stats::Stats>>,
 	) -> Result<Controller, Error> {
@@ -61,6 +160,7 @@ impl Controller {
 			server_url: server_url.to_string(),
 			server_login: server_login,
 			server_password: server_password,
+			server_tls_enabled: server_tls_enabled,
 			stream: None,
 			tx: tx,
 			rx: rx,
@@ -71,14 +171,12 @@ impl Controller {
 	}
 
 	pub fn try_connect(&mut self) -> Result<(), Error> {
-		match TcpStream::connect(self.server_url.clone()) {
-			Ok(conn) => {
-				let _ = conn.set_nonblocking(true);
-				self.stream = Some(BufStream::new(conn));
-				Ok(())
-			}
-			Err(e) => Err(Error::ConnectionError(format!("{}", e))),
-		}
+		self.stream = Some(Stream::new());
+		self.stream
+			.as_mut()
+			.unwrap()
+			.try_connect(&self.server_url, self.server_tls_enabled)?;
+		Ok(())
 	}
 
 	fn read_message(&mut self) -> Result<Option<String>, Error> {
@@ -112,12 +210,14 @@ impl Controller {
 			return Err(Error::ConnectionError(String::from("No server connection")));
 		}
 		debug!(LOGGER, "sending request: {}", message);
-		let _ = self.stream
+		let _ = self
+			.stream
 			.as_mut()
 			.unwrap()
 			.write(message.as_bytes())
 			.unwrap();
-		let _ = self.stream
+		let _ = self
+			.stream
 			.as_mut()
 			.unwrap()
 			.write("\n".as_bytes())
@@ -184,7 +284,14 @@ impl Controller {
 		self.send_message(&req_str)
 	}
 
-	fn send_message_submit(&mut self, height: u64, job_id: u64, edge_bits: u32, nonce: u64, pow: Vec<u64>) -> Result<(), Error> {
+	fn send_message_submit(
+		&mut self,
+		height: u64,
+		job_id: u64,
+		edge_bits: u32,
+		nonce: u64,
+		pow: Vec<u64>,
+	) -> Result<(), Error> {
 		let params_in = types::SubmitParams {
 			height: height,
 			job_id: job_id,
@@ -313,7 +420,7 @@ impl Controller {
 					if result.contains("blockfound") {
 						info!(LOGGER, "Block Found!!");
 						stats.client_stats.last_message_received =
-						format!("Last Message Received: Block Found!!");
+							format!("Last Message Received: Block Found!!");
 					}
 				} else {
 					let err = res.error.unwrap();
@@ -376,7 +483,6 @@ impl Controller {
 		// Request the first job template
 		thread::sleep(std::time::Duration::from_secs(1));
 		let mut was_disconnected = true;
-
 		loop {
 			// Check our connection status, and try to correct if possible
 			if let None = self.stream {
