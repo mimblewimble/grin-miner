ee4ffe3a6b5260a4cdb03b9ca3aeb0c9b3a0024d
yeastplume <yeastplume@protonmail.com>
2019-01-04 11:37:51 +0000

    release building

--- .auto-release.sh
+++ .auto-release.sh
@@ -21,9 +21,11 @@ echo 'make a tarball for the release binary...\n'
 #    exit 0
 #else
     # Do some custom requirements on Linux
-    cd target/release ; rm -f *.tgz; tar zcf "grin-miner-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz" grin-miner
+    mkdir deploy; cp grin-miner.toml deploy; cp -R target/release/plugins deploy/plugins
+    cp target/release/grin-miner deploy
+    cd deploy ; rm -f *.tgz; tar zcf "grin-miner-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz" *
     /bin/ls -ls *.tgz  | awk '{print $6,$7,$8,$9,$10}'
-    md5sum "grin-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz" > "grin-miner-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz"-md5sum.txt
+    md5sum "grin-miner-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz" > "grin-miner-$tagname-$TRAVIS_JOB_ID-linux-amd64.tgz"-md5sum.txt
     /bin/ls -ls *-md5sum.txt  | awk '{print $6,$7,$8,$9,$10}'
     cd - > /dev/null;
     echo "tarball generated\n"
@@ -33,34 +35,34 @@ version="$tagname"
 branch="`git symbolic-ref -q --short HEAD`"
 
 # automatic changelog generator
-gem install github_changelog_generator
+#gem install github_changelog_generator
 
-LAST_REVISION=$(git rev-list --tags --skip=1 --max-count=1)
-LAST_RELEASE_TAG=$(git describe --abbrev=0 --tags ${LAST_REVISION})
+#LAST_REVISION=$(git rev-list --tags --skip=1 --max-count=1)
+#LAST_RELEASE_TAG=$(git describe --abbrev=0 --tags ${LAST_REVISION})
 
 # Generate CHANGELOG.md
-github_changelog_generator \
-  -u $(cut -d "/" -f1 <<< $repo_slug) \
-  -p $(cut -d "/" -f2 <<< $repo_slug) \
-  --token $token \
-  --since-tag ${LAST_RELEASE_TAG}
+#github_changelog_generator \
+#  -u $(cut -d "/" -f1 <<< $repo_slug) \
+#  -p $(cut -d "/" -f2 <<< $repo_slug) \
+#  --token $token \
+#  --since-tag ${LAST_RELEASE_TAG}
 
-body="$(cat CHANGELOG.md)"
+#body="$(cat CHANGELOG.md)"
 
 # Overwrite CHANGELOG.md with JSON data for GitHub API
-jq -n \
-  --arg body "$body" \
-  --arg name "$version" \
-  --arg tag_name "$version" \
-  --arg target_commitish "$branch" \
-  '{
-    body: $body,
-    name: $name,
-    tag_name: $tag_name,
-    target_commitish: $target_commitish,
-    draft: false,
-    prerelease: false
-  }' > CHANGELOG.md
+#jq -n \
+#  --arg body "$body" \
+#  --arg name "$version" \
+#  --arg tag_name "$version" \
+#  --arg target_commitish "$branch" \
+#  '{
+#    body: $body,
+#    name: $name,
+#    tag_name: $tag_name,
+#    target_commitish: $target_commitish,
+#    draft: false,
+#    prerelease: false
+#  }' > CHANGELOG.md
 
 echo "Create release $version for repo: $repo_slug, branch: $branch"
 curl -H "Authorization: token $token" --data @CHANGELOG.md "https://api.github.com/repos/$repo_slug/releases"
