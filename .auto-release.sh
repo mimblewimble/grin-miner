3e31041b46baf6fc8514a49bde429b8035dfe87b
yeastplume <yeastplume@protonmail.com>
2019-01-04 21:11:54 +0000

    small packaging fix

--- .auto-release.sh
+++ .auto-release.sh
@@ -12,7 +12,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Do some custom requirements on OS X
     mkdir -p $deploy_dir; cp grin-miner.toml $deploy_dir; cp -R target/release/plugins $deploy_dir
-    cp target/release/grin-miner $deploy_dir
+    cp target/release/grin-miner "$deploy_dir/plugins"
     cd deploy ; rm -f *.tgz; tar zcf "grin-miner-$tagname-$TRAVIS_JOB_ID-osx.tgz" *
     /bin/ls -ls *.tgz  | awk '{print $6,$7,$8,$9,$10}'
     md5 "grin-miner-$tagname-$TRAVIS_JOB_ID-osx.tgz" > "grin-miner-$tagname-$TRAVIS_JOB_ID-osx.tgz"-md5sum.txt
