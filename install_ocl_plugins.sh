4bfa120eeab8a0329c835a8fbb433c2e8d6d3c07
Can Selcik <selcik.can@gmail.com>
2019-01-12 21:06:02 -0800

    Fixing issue with commented out miner_plugin_dir being unaccounted for

--- install_ocl_plugins.sh
+++ install_ocl_plugins.sh
@@ -1,13 +1,16 @@
-plugins_dir=`grep miner_plugin_dir grin-miner.toml |  cut -d "\"" -f2 | cut -d "\"" -f1`
-if [ -z "$plugins_dir" ];
-	then plugins_dir="target/debug/plugins"
+plugins_dir=$(egrep '^miner_plugin_dir' grin-miner.toml | awk '{ print $NF }' | xargs echo)
+if [ -z "$plugins_dir" ]; then
+	plugins_dir="target/debug/plugins"
 fi
+
+# Install ocl_cuckatoo
 cd ocl_cuckatoo
 cargo build --release
 cd ..
 cp target/release/libocl_cuckatoo.so $plugins_dir/ocl_cuckatoo.cuckooplugin
+
+# Install ocl_cuckaroo
 cd ocl_cuckaroo
 cargo build --release
 cd ..
 cp target/release/libocl_cuckaroo.so $plugins_dir/ocl_cuckaroo.cuckooplugin
-
