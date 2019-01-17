8a2c2d7f6bb0e469e7d1c6cf84055ddec556f07e
Pau <pau@dabax.net>
2019-01-17 14:04:08 +0100

    Fix install_ocl_plugin.sh to point release directory by default (#132)
    Signed-off-by: p4u <p4u@dabax.net>

--- install_ocl_plugins.sh
+++ install_ocl_plugins.sh
@@ -1,6 +1,6 @@
 plugins_dir=$(egrep '^miner_plugin_dir' grin-miner.toml | awk '{ print $NF }' | xargs echo)
 if [ -z "$plugins_dir" ]; then
-	plugins_dir="target/debug/plugins"
+	plugins_dir="target/release/plugins"
 fi
 mkdir -p "$plugins_dir";
 
